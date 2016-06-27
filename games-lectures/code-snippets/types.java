af17c4b41946b3aa76ad04b07d8865636ff50927
Calem Bendell <calem.j.bendell@gmail.com>
2016-06-26 23:14:38 -0400

    update slides for day 1

---
+++ games-lectures/code-snippets/types.java
@@ -0,0 +1,4 @@
+int aNumber = 54920;
+double aDecimalNumber = 43902.3290;
+String aSentence = "Hello world!";
+char ACharacter = 'c';
---
+++ games-lectures/code-snippets/variables.java
@@ -0,0 +1,6 @@
+int five; // tells the computer to make space
+            // for a number we called number
+
+five = 5; // now the value of the "five" is 0
+five = 6; // now it's 6....
+five = 5; // let's change it back to 5
